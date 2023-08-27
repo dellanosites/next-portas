3fb25c9eda3fbb5e0cd0081b72ba734fc7fefd73
Caio <bladellano@gmail.com>
2023-08-27 11:25:36 +0100

    feat: Criando modelo porta.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,11 @@ import Porta from "@/components/Porta";
 
 export default function Home() {
   return (
-    <div>
-      <Porta/>
+    <div style={{
+      display: "flex"
+    }}>
+      <Porta selecionada />
+      <Porta />
     </div>
   )
 }
