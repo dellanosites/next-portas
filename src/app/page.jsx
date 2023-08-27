77e1567e1505dbafa99e6fea60cb481a3bcf603a
Caio <bladellano@gmail.com>
2023-08-27 11:49:03 +0100

    feat: Add typescript, usando modelo porta e ajustando componentes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
-import Porta from "@/components/Porta";
+'use client'
+
+import { useState } from "react";
+import Porta from "../components/Porta";
+import PortaModel from '../model/porta'
 
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1))
+
   return (
-    <div style={{
-      display: "flex"
-    }}>
-      <Porta selecionada />
-      <Porta />
+    <div style={{ display: "flex" }}>
+      <Porta porta={p1} />
     </div>
   )
 }
