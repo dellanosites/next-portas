3e380403518efe33ee0bea4c65663dacff49c6f8
Caio <bladellano@gmail.com>
2023-08-27 10:48:48 +0100

    chore: Adicionando porta

---
+++ src/components/Porta.jsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/Porta.module.css"
+import Presente from "./Presente";
+
+function Porta(props) {
+    return ( 
+        <div className={styles.porta}>
+            <div className={styles.numero}>3</div>
+        </div>
+     );
+}
+
+export default Porta;
