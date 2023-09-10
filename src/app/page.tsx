cf9ea9f152a778768a27cd3a43762b4910f49133
Caio <bladellano@gmail.com>
2023-09-10 18:14:16 +0100

    fix: Ajustando extensao do arquivo, css e etc

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Cartao from '../components/Cartao'
+import styles from '../styles/Formulario.module.css'
+import Link from 'next/link'
+
+export default function Formulario() {
+
+  return (
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor='#c0392c'>
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao />
+      </div>
+      <div>
+        <Cartao />
+        <Cartao bgcolor='#28a085'>
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+    </div>
+  )
+}
